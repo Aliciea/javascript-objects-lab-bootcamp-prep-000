912615fa3a96f98f10f4c37b7669d52b3c6821d4
Aliciea D Suechin <adr330@nyu.edu>
2019-06-06 15:54:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var object = {
   object: 'recipes',
 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value 
+  newObject = Object.assign({}, object)
+  return object[key]
+}
